ec47edcc17a15890ba3f30a7c9db6da17f1c9aef
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-13 10:42:06 -0500

    Integración con cliente-service haciendo petición para encontrar un cliente por su id

--- src/main/java/com/electrodostore/carrito_service/integration/cliente/ClienteIntegrationService.java
+++ src/main/java/com/electrodostore/carrito_service/integration/cliente/ClienteIntegrationService.java
@@ -1,4 +1,40 @@
 package com.electrodostore.carrito_service.integration.cliente;
 
+import com.electrodostore.carrito_service.exception.ServiceUnavailable;
+import com.electrodostore.carrito_service.integration.cliente.client.ClienteFeignClient;
+import com.electrodostore.carrito_service.integration.cliente.dto.ClienteIntegrationDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j  //@Slf4j contiene un logger para agregar errores o warnings informativos al log del proyecto
+@Service
+//Clase donde se van a definir cada uno de los métodos protegidos que hacen peticiones a cliente-service por medio de ClienteFeignClient
+//Este service actúa como intermediario entre la lógica de negocio de carrito-service y el cliente Feign de cliente-service
 public class ClienteIntegrationService {
+
+    //Inyección de dependencia por constructor para el cliente Feign de cliente-service
+    private final ClienteFeignClient clienteFeignClient;
+    public ClienteIntegrationService(ClienteFeignClient clienteFeignClient){
+        this.clienteFeignClient = clienteFeignClient;
+    }
+
+    /*Método protegido con Circuit-Breaker el cuál actúa como un circuito que después de una serie de errores seguidos de
+     infraestructura en la comunicación con cliente-service se abre y me redirige las peticiones al método fallback durante
+      un tiempo determinado antes de volver a intentar comunicarse*/
+    @CircuitBreaker(name = "cliente-service", fallbackMethod = "findClienteFallback")
+    public ClienteIntegrationDto findCliente(Long clienteId){
+        return clienteFeignClient.findCliente(clienteId);
+    }
+
+    /*Método fallback para el método findCliente, este fallback actúa como un plan-B en caso de que ocurra un error de
+     infraestructura en la petición a cliente-service. La función de este es informar sobre el error de comunicación al cliente*/
+    public ClienteIntegrationDto findClienteFallback(Long clienteId, Throwable ex){
+
+        //Informamos la activación del fallback
+        log.warn("fallback activado para el método findCliente. clienteId={}", clienteId, ex);
+
+        //Lanzamos excepción indicando el problema
+        throw new ServiceUnavailable("No fue posible establecer la comunicación con cliente-service. Intente de nuevo más tarde");
+    }
 }
--- src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignClient.java
+++ src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignClient.java
@@ -1,4 +1,15 @@
 package com.electrodostore.carrito_service.integration.cliente.client;
 
-public class ClienteFeignClient {
+import com.electrodostore.carrito_service.integration.cliente.dto.ClienteIntegrationDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//Interface FeignClient donde se va a describir y declarar cada end-point perteneciente a un determinado método en cliente-service y los parámetros requeridos por este
+@FeignClient(name = "cliente-service") //Mismo nombre con el que se registró el servicio en eureka-server
+public interface ClienteFeignClient {
+
+    //Descripción del método que busca y trae un cliente desde el servicio cliente
+    @GetMapping("/clientes/{clienteId}")
+    ClienteIntegrationDto findCliente(@PathVariable Long clienteId);
 }

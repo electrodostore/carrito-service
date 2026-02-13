7780f5cf668f79eae9928a4cf5e416927df7aba4
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-13 18:49:38 -0500

    Configuración del FeignClient que hace peticiones a cliente-service

--- src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignClient.java
+++ src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignClient.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //Interface FeignClient donde se va a describir y declarar cada end-point perteneciente a un determinado método en cliente-service y los parámetros requeridos por este
-@FeignClient(name = "cliente-service") //Mismo nombre con el que se registró el servicio en eureka-server
+@FeignClient(name = "cliente-service",  //Mismo nombre con el que se registró el servicio en eureka-server
+        configuration = ClienteFeignConfig.class)  //Definimos la configuración para este FeignClient
 public interface ClienteFeignClient {
 
     //Descripción del método que busca y trae un cliente desde el servicio cliente
--- src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignConfig.java
+++ src/main/java/com/electrodostore/carrito_service/integration/cliente/client/ClienteFeignConfig.java
@@ -1,4 +1,17 @@
 package com.electrodostore.carrito_service.integration.cliente.client;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Clase de configuración para el FeignClient que se encarga de hacer peticiones a cliente-service
+@Configuration
 public class ClienteFeignConfig {
+
+    /*Definimos el Bean que Spring registra para que Spring-Cloud-OpenFeign utilice como el errorDecoder del FeignClient
+    * que crea para hacer peticiones a cliente-service*/
+    /*Cuandó Feign reciba una response con statusCode diferente de 2xx, este revisará el errorDecoder que se le configuró
+    para tratar de interpretar y transformar la response para retornarla a Feign ya sea como excepción de dominio o FeignException
+    basada en la Response*/
+    @Bean
+    public ClienteErrorDecoder feignErrorDecoder(){return new ClienteErrorDecoder();}
 }

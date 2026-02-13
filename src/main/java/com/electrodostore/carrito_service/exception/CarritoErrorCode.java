afa5dd569cbf85145c2acd57332a80aab37d6a9c
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-13 12:04:43 -0500

    Enum para almacenar los diferentes errorCodes de cada excepción de dominio

---
+++ src/main/java/com/electrodostore/carrito_service/exception/CarritoErrorCode.java
@@ -0,0 +1,10 @@
+package com.electrodostore.carrito_service.exception;
+
+//Enum encargado de encapsular y guardar el conjunto de valores de errorCodes que identifican a cada excepción de negocio fuera de este dominio
+/*También podemos usar estos valores de errorCodes para compararlos con un errorCode que venga en una response de error y
+ así interpretar y transformar esa response a la excepción de dominio correspondiente*/
+public enum CarritoErrorCode {
+
+    CLIENTE_NOT_FOUND  //ErrorCode de ClienteNotFoundException
+
+}
--- src/main/java/com/electrodostore/carrito_service/exception/ClienteNotFoundException.java
+++ src/main/java/com/electrodostore/carrito_service/exception/ClienteNotFoundException.java
@@ -1,10 +1,19 @@
 package com.electrodostore.carrito_service.exception;
 
+import lombok.Getter;
+
+@Getter //Exponemos campo(s)
 //Excepción personalizada para este dominio para cuando no se logre encontrar un Cliente consultado al servicio Ciente
 public class ClienteNotFoundException extends RuntimeException {
+
+    //ErrorCode identificativo para esta excepción fuera del dominio
+    private final CarritoErrorCode errorCode;
 
     //Subimos mensaje para tenerlo disponible con el método getMessage()
     public ClienteNotFoundException(String message) {
         super(message);
+
+        //Se le asigna el valor correspondiente del conjunto de valores guardados en CarritoErrorCode
+        this.errorCode = CarritoErrorCode.CLIENTE_NOT_FOUND;
     }
 }

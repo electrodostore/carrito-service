3903e916f002018251651ae9fbf9e4b2fcf3db0f
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-13 10:34:38 -0500

    Dependencias para Circuit Breaker y FeignClient

--- src/main/java/com/electrodostore/carrito_service/CarritoServiceApplication.java
+++ src/main/java/com/electrodostore/carrito_service/CarritoServiceApplication.java
@@ -2,8 +2,12 @@ package com.electrodostore.carrito_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableDiscoveryClient
 public class CarritoServiceApplication {
 
 	public static void main(String[] args) {

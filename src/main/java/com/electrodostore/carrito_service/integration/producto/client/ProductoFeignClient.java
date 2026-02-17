669635b0ab73312d5319fd57cd820aeb43939d57
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-17 17:22:09 -0500

    Descripción de los métodos de consulta de producto(s) a producto-service

--- src/main/java/com/electrodostore/carrito_service/integration/producto/client/ProductoFeignClient.java
+++ src/main/java/com/electrodostore/carrito_service/integration/producto/client/ProductoFeignClient.java
@@ -1,4 +1,25 @@
 package com.electrodostore.carrito_service.integration.producto.client;
 
-public class ProductoFeignClient {
+import com.electrodostore.carrito_service.integration.producto.dto.ProductoIntegrationDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+//Clase descriptiva de cada uno de los métodos para hacer peticiones al servicio Producto
+@FeignClient(name = "producto-service") //Mismo nombre con el que se registró en eureka-server
+public interface ProductoFeignClient {
+
+    //Descripción del método para consultar una lista de productos por sus ids
+    @PostMapping("/productos/traer-productos-por-ids")
+    List<ProductoIntegrationDto> findProductos(@RequestBody List<Long> productosIds);
+
+    //Descripción del método para consultar un producto por su id
+    @GetMapping("/productos/{productoId}")
+    ProductoIntegrationDto findProducto(@PathVariable Long productoId);
+
+
 }

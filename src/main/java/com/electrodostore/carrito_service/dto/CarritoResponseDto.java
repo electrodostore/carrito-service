cda0d34ddfb2fd87e3bb1f8eda4d7e46a16f088b
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-19 17:16:50 -0500

    Métodos Getter, Setter y constructor de clase DTO

--- src/main/java/com/electrodostore/carrito_service/dto/CarritoResponseDto.java
+++ src/main/java/com/electrodostore/carrito_service/dto/CarritoResponseDto.java
@@ -4,10 +4,17 @@ import com.electrodostore.carrito_service.model.CarritoStatus;
 import com.electrodostore.carrito_service.model.ClienteSnapshot;
 import com.electrodostore.carrito_service.model.ProductoSnapshot;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter  @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 //Clase DTO para exponer los datos del carrito de un determinado cliente, asi como los productos que hay en él
 public class CarritoResponseDto {
 

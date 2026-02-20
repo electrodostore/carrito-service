2fe416857b54fe397bb2181a532e83b702f17175
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-20 16:26:51 -0500

    Método de consulta de productos en el service y correción de inyección de dependencia para ProductoIntegrationService

--- src/main/java/com/electrodostore/carrito_service/service/CarritoService.java
+++ src/main/java/com/electrodostore/carrito_service/service/CarritoService.java
@@ -5,8 +5,11 @@ import com.electrodostore.carrito_service.dto.CarritoResponseDto;
 import com.electrodostore.carrito_service.dto.ClienteResponseDto;
 import com.electrodostore.carrito_service.dto.ProductoResponseDto;
 import com.electrodostore.carrito_service.exception.CarritoNotFoundException;
+import com.electrodostore.carrito_service.exception.ProductoNotFoundException;
 import com.electrodostore.carrito_service.integration.cliente.ClienteIntegrationService;
 import com.electrodostore.carrito_service.integration.cliente.dto.ClienteIntegrationDto;
+import com.electrodostore.carrito_service.integration.producto.ProductoIntegrationService;
+import com.electrodostore.carrito_service.integration.producto.dto.ProductoIntegrationDto;
 import com.electrodostore.carrito_service.model.Carrito;
 import com.electrodostore.carrito_service.model.ClienteSnapshot;
 import com.electrodostore.carrito_service.model.ProductoSnapshot;
@@ -14,9 +17,7 @@ import com.electrodostore.carrito_service.repository.ICarritoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 import static com.electrodostore.carrito_service.model.CarritoStatus.PENDING;
 
@@ -28,11 +29,43 @@ public class CarritoService implements ICarritoService {
     private final ICarritoRepository carritoRepo;
     //Inyección de dependencia para la integración con cliente-service
     private final ClienteIntegrationService clienteIntegration;
+    private final ProductoIntegrationService productoIntegration;
 
     //Inyección por método constructor
-    public CarritoService(ICarritoRepository carritoRepo, ClienteIntegrationService clienteIntegration) {
+    public CarritoService(ICarritoRepository carritoRepo, ClienteIntegrationService clienteIntegration, ProductoIntegrationService productoIntegration) {
         this.carritoRepo = carritoRepo;
         this.clienteIntegration = clienteIntegration;
+        this.productoIntegration = productoIntegration;
+    }
+
+    //Método propio para hacer la integración con productoService y consultar uno o varios productos por su id
+    //En caso de que ocurran problemas en la integración (algún producto no fue encontrado, no se mandó a consultar nada, etc) también es manejarán aquí
+    private List<ProductoIntegrationDto> findProductos(Set<Long> productosIds){
+
+        //En caso de que no se mande ningún ID de producto en la lista, no podemos hacer la integración
+        if(productosIds.isEmpty()){throw new ProductoNotFoundException("Ningún producto fue mandado a consultar");}
+
+        //Si solo se manda a consultar un producto, no es necesario usar el método de integración que consulta una lista de estos
+        //Set no me permite acceder a los elementos de la lista, por eso toca hacer un casting a List para sacar el id del producto
+        if(productosIds.size() == 1){return List.of(productoIntegration.findProducto(new ArrayList<>(productosIds).get(0)));}
+
+        //Si son varios los productos sacamos la lista de estos
+        List<ProductoIntegrationDto> productosIntegration = findProductos(productosIds);
+
+        //Comparamos longitudes de la lista de ids con la lista de productos que llegaron en la integración para determinar si llegaron todos
+        if(productosIntegration.size() < productosIds.size()){throw new ProductoNotFoundException("Uno o varios productos no fueron encontrado");}
+
+        //Finalmente, si todo está ok -> Retornamos lista
+        return productosIntegration;
+    }
+
+    /*Método propio para transferir los datos de una lista de productos, que se integraron desde producto-service este
+         servicio, a una lista de objetos Snapshot para su posterior persistencia en la base de datos*/
+    private List<ProductoSnapshot> productosIntegrationToSnapshot(List<ProductoIntegrationDto> productosIntegration){
+        //Lista de Snapshots de los productos que se integraron
+        List<ProductoSnapshot> productosSnapshot = new ArrayList<>();
+
+        return null;
     }
 
     //Método propio cuya función es preparar un Cliente para ser guardado como parte del registro de un carrito en la base de datos

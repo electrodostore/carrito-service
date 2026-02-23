b5a66c2498fe960c8004e5bbc56054c9e206f633
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-23 16:41:20 -0500

    End-Point para que el cliente pueda usar el método 'agregarProductos()'

--- src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
+++ src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
@@ -2,6 +2,7 @@ package com.electrodostore.carrito_service.controller;
 
 import com.electrodostore.carrito_service.dto.CarritoCreadoResponseDto;
 import com.electrodostore.carrito_service.dto.CarritoResponseDto;
+import com.electrodostore.carrito_service.dto.ProductoAgregarDto;
 import com.electrodostore.carrito_service.service.ICarritoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -30,5 +31,10 @@ public class CarritoController {
     @PostMapping
    public ResponseEntity<CarritoCreadoResponseDto> crearCarrito(@RequestBody Long clienteId){
        return ResponseEntity.ok(carritoService.crearCarrito(clienteId));
+   }
+
+   @PatchMapping("/agregar-productos/{carritoId}")
+    public ResponseEntity<CarritoResponseDto> agregarProductos(@PathVariable Long carritoId, @RequestBody List<ProductoAgregarDto> listProductos){
+       return ResponseEntity.ok(carritoService.agregarProductos(carritoId, listProductos));
    }
 }

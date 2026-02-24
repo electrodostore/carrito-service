fcc5ef9607a776df4bb4374ccd1358f48dff8ff6
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-24 18:08:34 -0500

    End-point para acceder al método de eliminar productos de carrito

--- src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
+++ src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
@@ -38,4 +38,10 @@ public class CarritoController {
     public ResponseEntity<CarritoResponseDto> agregarProductos(@PathVariable Long carritoId, @RequestBody List<ProductoAgregarDto> listProductos){
        return ResponseEntity.ok(carritoService.agregarProductos(carritoId, listProductos));
    }
+
+   //Es un método DELETE ya que se está eliminando un recurso (productos) dentro del carrito
+   @DeleteMapping("/eliminar-productos/{carritoId}")
+    public ResponseEntity<CarritoResponseDto> eliminarProductos(@PathVariable Long carritoId, @RequestBody List<Long> productosEliminarIds){
+       return ResponseEntity.ok(carritoService.deleteProductos(carritoId, productosEliminarIds));
+   }
 }

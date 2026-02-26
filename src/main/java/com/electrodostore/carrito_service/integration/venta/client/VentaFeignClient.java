025a23a321a4cd777c5eb4015878bc2b3e79a1ce
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-26 16:42:32 -0500

    Definición y asignación de configuración al FeignClient que hace peticiones a venta-service

--- src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignClient.java
+++ src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignClient.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 //Clase cliente Feign de venta-service y por medio de la cual se van a hacer las peticiones a ese servicio
-@FeignClient(name = "venta-service") //mismo nombre registrado en Eureka-Server
+@FeignClient(name = "venta-service",  //mismo nombre registrado en Eureka-Server
+        configuration = VentaFeignConfig.class //Definimos la configuración que este FeignClient va a tener
+)
 public interface VentaFeignClient {
 
     //Descripción del método que guarda una venta a partir de los datos del carrito que se compró
--- src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignConfig.java
+++ src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignConfig.java
@@ -1,4 +1,14 @@
 package com.electrodostore.carrito_service.integration.venta.client;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//Clase de configuración que se asocia con el FeignClient que se crea para hacer peticiones a venta-service
+@Configuration
 public class VentaFeignConfig {
+
+    /*Registramos el Bean del errorDecoder de venta-service en el contenedor de Spring para Feign lo encuentre allí y lo
+    asocie con el FeignClient que hace peticiones a venta-service cuando lo cree¨*/
+    @Bean
+    public VentaErrorDecoder errorDecoder(){return new VentaErrorDecoder();}
 }

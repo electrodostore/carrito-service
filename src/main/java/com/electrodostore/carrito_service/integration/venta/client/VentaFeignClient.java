91c59c99fbd73c298bbee5645af0fe6607502cc3
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-26 15:01:34 -0500

    Definición de FeignClient que va a hacer peticiones a venta-service y descripción del método que registra ventas

--- src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignClient.java
+++ src/main/java/com/electrodostore/carrito_service/integration/venta/client/VentaFeignClient.java
@@ -1,4 +1,17 @@
 package com.electrodostore.carrito_service.integration.venta.client;
 
-public class VentaFeignClient {
+import com.electrodostore.carrito_service.integration.venta.dto.VentaIntegrationRequestDto;
+import com.electrodostore.carrito_service.integration.venta.dto.VentaIntegrationResponseDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+//Clase cliente Feign de venta-service y por medio de la cual se van a hacer las peticiones a ese servicio
+@FeignClient(name = "venta-service") //mismo nombre registrado en Eureka-Server
+public interface VentaFeignClient {
+
+    //Descripción del método que guarda una venta a partir de los datos del carrito que se compró
+    @PostMapping("/ventas")
+    VentaIntegrationResponseDto createVenta(@RequestBody VentaIntegrationRequestDto nuevaVenta);
+
 }

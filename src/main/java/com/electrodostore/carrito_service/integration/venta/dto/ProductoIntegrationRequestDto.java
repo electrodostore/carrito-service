406bb433ea0c9c7541e8aabab36b5080b5f90a76
Emanuel <emanuelatencia1126654@correo.itm.edu.co>
2026-02-27 16:04:08 -0500

    Métodos getter, setter y constructor para movilización interna de los datos del DTO

--- src/main/java/com/electrodostore/carrito_service/integration/venta/dto/ProductoIntegrationRequestDto.java
+++ src/main/java/com/electrodostore/carrito_service/integration/venta/dto/ProductoIntegrationRequestDto.java
@@ -1,5 +1,13 @@
 package com.electrodostore.carrito_service.integration.venta.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter  @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 //DTO que se usará para almacenar los datos que se van a transferir a los productos que se registrán en la venta una vez se decida comprar el carrito
 public class ProductoIntegrationRequestDto {
 

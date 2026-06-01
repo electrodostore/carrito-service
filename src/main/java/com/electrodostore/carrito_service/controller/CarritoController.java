5ddef0c73a19a50be8453a90327f50794e1d1a8b
Emanuel <emanuelatencia2407@gmail.com>
2026-06-01 12:54:08 -0500

    feat: implementar control de acceso basado en roles y autenticación

--- src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
+++ src/main/java/com/electrodostore/carrito_service/controller/CarritoController.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,42 +22,50 @@ public class CarritoController {
     private final ICarritoService carritoService;
     public CarritoController(ICarritoService carritoService){this.carritoService = carritoService;}
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping
     public ResponseEntity<List<CarritoResponseDto>> findAllCarritos(){
         return ResponseEntity.ok(carritoService.findAllCarritos());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{carritoId}")
     public ResponseEntity<CarritoResponseDto> findCarrito(@PathVariable Long carritoId){
         return ResponseEntity.ok(carritoService.findCarritoResponse(carritoId));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping("/me")
     public ResponseEntity<CarritoResponseDto> findMyCarrito(){
         return ResponseEntity.ok(carritoService.findMyCarritoPending());
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/me/productos")
     public ResponseEntity<CarritoResponseDto> agregarProductos(@RequestBody @NotEmpty List<@NotNull @Valid ProductoAgregarDto> listProductos){
         return ResponseEntity.ok(carritoService.agregarProductos(listProductos));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/me/productos/{productoEliminarId}")
     public ResponseEntity<CarritoResponseDto> eliminarProductos(@PathVariable Long productoEliminarId){
         return ResponseEntity.ok(carritoService.deleteProductos(productoEliminarId));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PatchMapping("/me/productos")
     public ResponseEntity<CarritoResponseDto> cambiarCantidadProducto(@RequestBody @Valid ProductoCambiarCantidadDto productoNuevaCantidad){
         return ResponseEntity.ok(carritoService.cambiarCantidadProducto(productoNuevaCantidad));
     }
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping("/me/comprar")
     public ResponseEntity<VentaIntegrationResponseDto> comprarCarrito(){
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(carritoService.comprarCarrito());
     }
 
+    @PreAuthorize("isAuthenticated()")
     @DeleteMapping("/me/productos")
     public ResponseEntity<Void> vaciarCarrito(){
         carritoService.vaciarMiCarrito();
